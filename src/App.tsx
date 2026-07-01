63c44b5fa67c73dc5a081d4a9d86cf956b1b68f3
Muhammad Naveed <naveed@codehuntspk.com>
2026-07-01 13:49:06 +0500

    feat: adding scroll to top on all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Courses from './pages/Courses'
@@ -6,6 +7,12 @@ import CourseDetail from './pages/CourseDetail'
 import NotFound from './pages/NotFound'
 
 export default function App() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
+  }, [pathname])
+
   return (
     <Layout>
       <Routes>
